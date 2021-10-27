30b22df608bc066107887a9e6e091214d3300704
Iurrii <1513it@gmail.com>
2021-10-27 20:18:13 +0200

    55

--- script.js
+++ script.js
@@ -198,26 +198,8 @@ function dateEngine() {
 
 
 
-// Your CSS as text
-var styles = `
-    .qwebirc-qui .ircwindow div { 
-        font-family: Georgia,Cambria,"Times New Roman",Times,serif;
-        margin: 26px auto 0 auto;
-        max-width: 650px;
-    }
-    .qwebirc-qui .lines {
-        font-size: 18px;
-        line-height: 1.58;
-        letter-spacing: -.004em;
-    }
-
-    .qwebirc-qui .nicklist a {
-        margin: 6px;
-    }
-`
 
 
-var styleSheet = document.createElement("style")
-styleSheet.type = "text/css"
-styleSheet.innerText = styles
-document.head.appendChild(styleSheet)
+
+
+
